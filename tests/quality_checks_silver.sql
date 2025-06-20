830f7e0f5d280617c762093e5d4a3be84e00052a
PhuriphatSei <138122611+PhuriphatSei@users.noreply.github.com>
2025-06-20 11:00:40 +0700

    Create quality_checks_silver.sql

---
+++ tests/quality_checks_silver.sql
@@ -0,0 +1,9 @@
+EXEC silver.load_silver;
+
+SELECT * FROM silver.aapl;
+SELECT * FROM silver.amzn;
+SELECT * FROM silver.googl;
+SELECT * FROM silver.meta;
+SELECT * FROM silver.msft;
+SELECT * FROM silver.nvda;
+SELECT * FROM silver.tsla;
