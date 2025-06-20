901dee81155682c6211a6b896bb94ed14737d0fa
PhuriphatSei <138122611+PhuriphatSei@users.noreply.github.com>
2025-06-20 11:01:38 +0700

    Create quality_checks_gold.sql

---
+++ tests/quality_checks_gold.sql
@@ -0,0 +1,7 @@
+SELECT * FROM gold.dim_aapl
+SELECT * FROM gold.dim_amzn;
+SELECT * FROM gold.dim_googl;
+SELECT * FROM gold.dim_meta;
+SELECT * FROM gold.dim_msft;
+SELECT * FROM gold.dim_nvda;
+SELECT * FROM gold.dim_tsla;
